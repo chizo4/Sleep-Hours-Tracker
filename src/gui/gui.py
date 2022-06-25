37742f4559830807b7f50e9585c75091bf6bd7ec
chizo <filip.cierkosz@gmail.com>
2022-06-26 00:01:20 +0100

    Added new sample from the matplotlib docs.

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -28,44 +28,103 @@ Author: Filip J. Cierkosz
 
 # root.mainloop()
 
+# import sys
+# from turtle import title
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+# from PyQt6.QtWidgets import QApplication, QWidget
+
+
+# class Canvas(FigureCanvas):
+#     def __init__(self, parent):
+#         fig, self.ax = plt.subplots(figsize=(5,4), dpi=100)
+#         super().__init__()
+#         self.setParent(parent)
+
+#         # Matplotlib script.
+
+#         t = np.arange(0.0, 2.0, 0.01)
+#         s = 1+np.sin(2*np.pi*t)
+
+#         self.ax.plot(t,s)
+
+#         self.ax.set(xlabel='time (s', ylabel='voltage (mV)', title='About as simple as it gets, folks')
+#         self.ax.grid()
+
+
+# class AppDemo(QWidget):
+#     def __init__(self):
+#         super().__init__()
+#         self.resize(1600, 800)
+
+#         chart = Canvas(self)
+
+# app = QApplication(sys.argv)
+# demo = AppDemo()
+# demo.show()
+# sys.exit(app.exec())
+
 import sys
-from turtle import title
+import time
+
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
-from PyQt6.QtWidgets import QApplication, QWidget
+
+from matplotlib.backends.qt_compat import QtWidgets
+from matplotlib.backends.backend_qtagg import (
+    FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
 
 
-class Canvas(FigureCanvas):
-    def __init__(self, parent):
-        fig, self.ax = plt.subplots(figsize=(5,4), dpi=100)
-        super().__init__()
-        self.setParent(parent)
-
-        # Matplotlib script.
-
-        t = np.arange(0.0, 2.0, 0.01)
-        s = 1+np.sin(2*np.pi*t)
-
-        self.ax.plot(t,s)
-
-        self.ax.set(xlabel='time (s', ylabel='voltage (mV)', title='About as simple as it gets, folks')
-        self.ax.grid()
-
-
-class AppDemo(QWidget):
+class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(1600, 800)
+        self._main = QtWidgets.QWidget()
+        self.setCentralWidget(self._main)
+        layout = QtWidgets.QVBoxLayout(self._main)
 
-        chart = Canvas(self)
+        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        # Ideally one would use self.addToolBar here, but it is slightly
+        # incompatible between PyQt6 and other bindings, so we just add the
+        # toolbar as a plain widget instead.
+        layout.addWidget(NavigationToolbar(static_canvas, self))
+        layout.addWidget(static_canvas)
 
-app = QApplication(sys.argv)
-demo = AppDemo()
-demo.show()
-sys.exit(app.exec())
+        dynamic_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        layout.addWidget(dynamic_canvas)
+        layout.addWidget(NavigationToolbar(dynamic_canvas, self))
+
+        self._static_ax = static_canvas.figure.subplots()
+        t = np.linspace(0, 10, 501)
+        self._static_ax.plot(t, np.tan(t), ".")
+
+        self._dynamic_ax = dynamic_canvas.figure.subplots()
+        t = np.linspace(0, 10, 101)
+        # Set up a Line2D.
+        self._line, = self._dynamic_ax.plot(t, np.sin(t + time.time()))
+        self._timer = dynamic_canvas.new_timer(50)
+        self._timer.add_callback(self._update_canvas)
+        self._timer.start()
+
+    def _update_canvas(self):
+        t = np.linspace(0, 10, 101)
+        # Shift the sinusoid as a function of time.
+        self._line.set_data(t, np.sin(t + time.time()))
+        self._line.figure.canvas.draw()
 
 
+if __name__ == "__main__":
+    # Check whether there is already a running QApplication (e.g., if running
+    # from an IDE).
+    qapp = QtWidgets.QApplication.instance()
+    if not qapp:
+        qapp = QtWidgets.QApplication(sys.argv)
+
+    app = ApplicationWindow()
+    app.show()
+    app.activateWindow()
+    app.raise_()
+    qapp.exec()
 
 
 
